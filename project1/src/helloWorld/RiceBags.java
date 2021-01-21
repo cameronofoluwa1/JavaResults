4caeb461c7a4bcf5ce12bc4596e1f8e6b8d025e2
Cameron-Ofoluwa <COfoluwa@qa.com>
2021-01-21 15:43:15 +0000

    Fixed rice bags programme

--- project1/src/helloWorld/RiceBags.java
+++ project1/src/helloWorld/RiceBags.java
@@ -1,32 +1,35 @@
 package helloWorld;
 
 public class RiceBags {
-	
+
 	public static void main(String[] args) {
-		
-		method1(5, 5, 5);
-		
+
+		method1(1, 1, 6);
+
 	}
-	
+
 	public static void method1(int SRB, int BRB, int rice) {
+
+		int newRiceValue = 0; // New calc value
+		int BRBU = 0; // Big rice bags used
+		int SRBU = 0; // Small rice bags used
+
+		for (int i = 0; i < BRB && rice >= 1; i++) {
+			if (Math.floor(rice / 5) >= 1) {
+				BRBU++;
+				System.out.println("Big rice bags used : " + BRBU);
+				rice = rice - 5;
+			}
+		}
+		for(int i = 0; i < SRB && rice >= 1; i++) {
+			if (rice > 0) {
+				SRBU++;
+				System.out.println("Small rice bags used : " + SRBU);
+				rice = rice - 1;
+			}
+		}
 		
-		int newRiceValue = 0; //New calc value
-		int BRBU = 0; //Big rice bags used
-		int SRBU = 0; //Small rice bags used
-		
-		if(rice % (BRB * 5) >= 0) {
-			BRBU = BRB;
-			newRiceValue = BRB * 5;
-			System.out.println("You've used " + BRBU + " big rice bags");
-			System.out.println("You have " + newRiceValue + "kg of rice packed");
-			if((rice - newRiceValue) - (SRB * 1) >= 0) {
-				SRBU = SRB;
-				newRiceValue = newRiceValue + (SRBU * 1);
-				System.out.println("You've used " + SRBU + " small rice bags");
-				System.out.println("You have " + newRiceValue + "kg of rice packed");
-				System.out.println("You've got " + (rice - newRiceValue) + "kg of rice left over");
-			}else {System.out.println("Not enough rice left over to fill the 1kg bags");}
-		}else {System.out.println("Not enough rice to fill the bags");}
+		System.out.println("Rice left over : " + rice + "kg");
 	}
 
 }
